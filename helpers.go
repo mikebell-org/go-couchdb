965b6f3f1b8c08a07b1423adc2d50bd0412e63d8
Mike Bell <mike@mikebell.org>
2013-03-01 20:56:12 -0800

    Turn on TCP keepalives to detect things like a dead changes feed

--- helpers.go
+++ helpers.go
@@ -2,11 +2,29 @@ package couchdb
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
+	"net"
 	"net/http"
 )
 
 var client http.Client
+
+func init() {
+	client = http.Client{Transport: &http.Transport{Dial: keepAliveDial}}
+}
+
+func keepAliveDial(nett, addr string) (c net.Conn, err error) {
+	if c, err = net.Dial(nett, addr); err != nil {
+		return
+	}
+	t, ok := c.(*net.TCPConn)
+	if !ok {
+		return c, fmt.Errorf("Socket returned by net.Dial is not a TCP socket, it's actually <%s>", c)
+	}
+	err = t.SetKeepAlive(true)
+	return
+}
 
 func jsonifyDoc(doc interface{}) (io.Reader, chan error) {
 	errCh := make(chan error)
