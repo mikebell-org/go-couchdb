1018b55a20211649866c9dc5aedb0b95846bdc29
Mike Bell <mike@mikebell.org>
2014-07-21 17:57:42 -0700

    Additional attachment support

--- documents.go
+++ documents.go
@@ -2,10 +2,20 @@ package couchdb
 
 import "time"
 
+type Attachment struct {
+	ContentType string `json:"content_type"`
+	RevPos      uint64 `json:"revpos"`
+	Digest      string `json:"digest"`
+	Length      uint64 `json:"length"`
+	Stub        bool   `json:"stub"`
+}
+
 // A minimal document you can embed into your document structs
 type BasicDocument struct {
-	ID  string `json:"_id,omitempty"`
-	Rev string `json:"_rev,omitempty"`
+	ID          string                `json:"_id,omitempty"`
+	Rev         string                `json:"_rev,omitempty"`
+	Deleted     bool                  `json:"_deleted,omitempty"`
+	Attachments map[string]Attachment `json:"_attachments,omitempty"`
 }
 
 // A more opinionated document you can base your structs off of.
