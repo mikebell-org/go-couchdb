5cbc7a0d647abcb36ec1c300894851d38280b58e
Mike Bell <mike@mikebell.org>
2015-11-17 16:03:00 -0800

    Move over to timestamp package

--- documents.go
+++ documents.go
@@ -1,6 +1,10 @@
 package couchdb
 
-import "time"
+import (
+	"time"
+
+	"github.com/mikebell-org/timestamp"
+)
 
 type Attachment struct {
 	ContentType string `json:"content_type"`
@@ -22,16 +26,12 @@ type BasicDocument struct {
 // This library will take care of updating the Created and Modified fields for you in conjunction with the CouchDocPreWrite method
 type BasicDocumentWithMtime struct {
 	BasicDocument
-	Created  float64 // Time document was created, expressed as decimal seconds since UNIX epoch
-	Modified float64 // Time document was last modified, expressed as decimal seconds since UNIX epoch
-}
-
-func floatTime(t time.Time) (r float64) {
-	return float64(t.Unix()) + (float64(t.Nanosecond()) / 1000000000.0)
+	Created  timestamp.Timestamp // Time document was created, expressed as decimal seconds since UNIX epoch
+	Modified timestamp.Timestamp // Time document was last modified, expressed as decimal seconds since UNIX epoch
 }
 
 func (d *BasicDocumentWithMtime) CouchDocPreWrite() {
-	now := floatTime(time.Now())
+	now := timestamp.New(time.Now())
 	if d.Created == 0 {
 		d.Created = now
 	}
